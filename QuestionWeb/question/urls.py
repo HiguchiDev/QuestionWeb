77e170f3f63886c6e76d0a3f0dca51172582e517
higuchi_dev <higuchi.dev@gmail.com>
2020-10-26 20:52:54 +0900

    問題をランダムで表示

--- QuestionWeb/question/urls.py
+++ QuestionWeb/question/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # 書籍
     path('question/<int:category_id>/<int:question_no>', views.QuestionView.as_view(), name='question'),
     path('category', views.CategoryView.as_view(), name='category'),
-    path('answer_result/<int:category_id>/<int:question_no>/<int:choice_no>', views.AnswerResultView.as_view(), name='answer_result'),
+    path('answer_result/<int:category_id>/<int:question_id>/<int:question_no>/<int:choice_no>', views.AnswerResultView.as_view(), name='answer_result'),
 ]
--- QuestionWeb/question/views.py
+++ QuestionWeb/question/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from question.models import Question, TextChoice, Category
+import random
 
 # Create your views here.
 
@@ -10,14 +11,41 @@ class QuestionView(TemplateView):
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
         
-        category = Category.objects.get(pk=self.kwargs.get('category_id'))
-        question = category.question_set.filter(question_no=self.kwargs.get('question_no'))
 
+        random_question_id_list = []        
+        category = Category.objects.get(pk=self.kwargs.get('category_id'))
+
+        if not self.request.session.get('question_id_list', False):
+            print('-------create session-----')
+            question_list = category.question_set.all()
+
+            while(len(random_question_id_list) < 5):
+                rand_int = random.randint(0, question_list.count() - 1)
+                rand_question = question_list[rand_int]
+
+                if self.__is_selected(random_question_id_list, rand_question) == False:
+                    random_question_id_list.append(rand_question.id)
+
+            self.request.session.set_expiry(0)
+            self.request.session['question_id_list'] = random_question_id_list
+            
         
-        ctx['question'] = question[0] # 基本一つしか取れないはず。
-        ctx['choice_list'] = question[0].textchoice_set.all().order_by('choice_no')
+        question_id_list = self.request.session.get('question_id_list')
+
+        question_id = question_id_list[self.kwargs.get('question_no') - 1]
+
+        ctx['question'] = Question.objects.get(pk=question_id)
+        ctx['category'] = category
 
         return ctx
+    
+    def __is_selected(self, random_question_id_list, question):
+        
+        for rand_question_id in random_question_id_list:
+            if rand_question_id == question.id:
+                return True
+
+        return False
 
 class CategoryView(TemplateView):
     template_name = "category.html"
@@ -37,13 +65,12 @@ class AnswerResultView(TemplateView):
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
         
-        question_list = Question.objects.filter(Category_id=self.kwargs.get('category_id'),
-                                                question_no=self.kwargs.get('question_no'))
-
-        answer_choice_no = question_list[0].answer_choice_no
+        question = Question.objects.get(pk=self.kwargs.get('question_id'))
+        answer_choice_no = question.answer_choice_no
 
         ctx['answer_choice_no'] = answer_choice_no
         ctx['choice_no'] = self.kwargs.get('choice_no')
-        ctx['question'] = question_list[0]
+        ctx['question'] = question
+        ctx['category_id'] = self.kwargs.get('category_id')
 
         return ctx
