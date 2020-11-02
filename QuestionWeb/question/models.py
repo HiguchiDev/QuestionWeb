333db47c767554f3e8d449fa6d2abad690b1f8ed
Takashi Higuchi <takashi.higuchi@jp.jig-saw.com>
2020-11-02 13:07:12 +0900

    微調整＆はみ出るバグ修正

--- QuestionWeb/question/models.py
+++ QuestionWeb/question/models.py
@@ -14,7 +14,8 @@ class Question(models.Model):
     Category = models.ManyToManyField(Category)
     question_no = models.PositiveSmallIntegerField(verbose_name='問題No.', default=0)
     answer_choice_no = models.PositiveSmallIntegerField(verbose_name='正解No.', default=1)
-    comment = models.CharField('解説', max_length=1024, default="")
+    comment = models.CharField('解説（正解の場合）', max_length=1024, default="")
+    
 
     def __str__(self):
         return self.body
